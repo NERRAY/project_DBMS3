d3f74acb3ea878f582a95557a1ed5fb87521bfc3
NERO <49379998+NERRAY@users.noreply.github.com>
2021-03-06 23:54:05 +0600

    Add files via upload

---
+++ FRIENDS-DML-data-load.sql
@@ -0,0 +1,34 @@
+ .mode csv books
+ .separator ,
+ .import books.csv books
+
+
+
+ .mode csv user
+ .separator ,
+ .import books.csv user
+
+
+
+ .mode csv location
+ .separator ,
+ .import books.csv location
+
+
+
+ .mode csv rating
+ .separator ,
+ .import books.csv rating
+
+
+ .mode csv login
+ .separator ,
+ .import books.csv login
+
+
+
+ .mode csv book_rec
+ .separator ,
+ .import books.csv book_rec
+
+
