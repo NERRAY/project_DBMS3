b25ec535ce59cf56233ae46334cc203f71f8a171
NERO <49379998+NERRAY@users.noreply.github.com>
2021-03-13 23:46:50 +0600

    Update 2.sql

--- queries/2.sql
+++ queries/2.sql
@@ -1,3 +1,3 @@
---Write a query to display the total number of books available in the library with alias name “NO_OF_BOOKS_AVAILABLE” (Which is not issued). 
+--Write a query to display the total number of books available in the library with alias name “NO_OF_BOOKS_AVAILABLE” . 
 
 SELECT COUNT(t1.BOOK_CODE)AS NO_OF_BOOKS_AVAILABLE FROM LMS_BOOK_DETAILS t1 LEFT JOIN LMS_BOOK_ISSUE t2 ON t1.BOOK_CODE = t2.BOOK_CODE WHERE t2.BOOK_CODE IS NULL;
